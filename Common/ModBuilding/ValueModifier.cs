427dc3a5355530388a420ed24ce6b1f4c34f0668
NycroV <83246959+NycroV@users.noreply.github.com>
2026-01-18 22:06:51 -0500

    Implicit ValueModifier casting

--- Common/ModBuilding/ValueModifier.cs
+++ Common/ModBuilding/ValueModifier.cs
@@ -156,10 +156,12 @@ public readonly struct ValueModifier(ValueModifierKind kind, float value) : IEqu
         _ => throw new IndexOutOfRangeException($"Invalid ValueModifierKind: {this.Kind}")
     };
 
-    public override string ToString() => $"ValueModifier[{kind},{value}]";
+    public override string ToString() => $"ValueModifier[{Kind},{Value}]";
     public bool Equals(ValueModifier other) => this.Kind == other.Kind && this.Value.Equals(other.Value);
     public override bool Equals(object? obj) => obj is ValueModifier other && this.Equals(other);
     public override int GetHashCode() => HashCode.Combine((int)this.Kind, this.Value);
     public static bool operator ==(ValueModifier left, ValueModifier right) => left.Equals(right);
     public static bool operator !=(ValueModifier left, ValueModifier right) => !left.Equals(right);
+    public static implicit operator ValueModifier(string representation) => FromJson(new JValue(representation));
+    public static implicit operator string(ValueModifier value) => (string)value.ToJToken()!;
 }
